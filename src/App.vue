935adcbdc03e532f036857768402ab85c9529aee
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-03-02 21:46:27 +0200

    added basis styles and utilities

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
-<template> </template>
+<template> <main-header></main-header></template>
 
-<style lang="scss">
-@import './styles/main.scss';
-</style>
+<style lang="scss"></style>
+
+<script>
+import MainHeader from './components/Header';
+export default {
+	components: { MainHeader },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+	<header>
+		<div class="wrapper content">
+			<div class="logo img-holder">
+				<img :src="require('@/assets/logo.png')" alt="logo" />
+			</div>
+		</div>
+	</header>
+</template>
