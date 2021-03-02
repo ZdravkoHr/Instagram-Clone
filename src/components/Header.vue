d71c0a21e2bfc534de1c366cd9bf194461299c06
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-03-02 23:21:14 +0200

    added searchbox and changed other stuff

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,29 @@
 <template>
 	<header>
-		<div class="wrapper content">
+		<div class="content-wrapper content">
 			<a href="#">
 				<div class="logo img-holder">
 					<img :src="require('@/assets/logo.png')" alt="logo" />
 				</div>
 			</a>
+			<search-box></search-box>
 		</div>
 	</header>
 </template>
 
+<script>
+import SearchBox from './SearchBox';
+export default {
+	components: {
+		SearchBox,
+	},
+};
+</script>
+
 <style lang="scss" scoped>
 header {
 	background-color: rgba(250, 250, 250, 1);
+	border-bottom: 1px solid $gray-1;
 }
 
 .content {
---
+++ src/components/SearchBox.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="wrapper">
+		<div class="search-icon">
+			<i class="fas fa-search"></i>
+		</div>
+		<input type="search" placeholder="Търсене" />
+		<div class="stop-icon">
+			<i class="fas fa-times-circle"></i>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.wrapper {
+	border: 1px solid $gray-1;
+	color: #b4a8a8;
+	border-radius: 3px;
+	display: flex;
+	align-items: center;
+	padding: 5px;
+
+	input {
+		border: 0;
+		outline: 0;
+		margin: 0 5px;
+	}
+
+	.search-icon {
+		font-size: 12px;
+		cursor: pointer;
+	}
+
+	.stop-icon {
+		font-size: 14px;
+		cursor: pointer;
+	}
+}
+</style>
