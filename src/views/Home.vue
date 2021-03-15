1144bb606822fa2122858bcac279d8b995591877
Zdravko Hristov <zdravkohristov0@gmail.com>
2021-03-15 13:20:50 +0200

    Aside component created

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,15 +4,17 @@
 			<stories-area class="stories-area"></stories-area>
 			<div class="feed"></div>
 		</div>
-		<aside></aside>
+		<aside-view></aside-view>
 	</main>
 </template>
 
 <script>
 import StoriesArea from '@/components/StoriesArea';
+import AsideView from '@/components/Aside';
 export default {
 	components: {
 		StoriesArea,
+		AsideView,
 	},
 };
 </script>
@@ -22,5 +24,6 @@ main {
 	display: grid;
 	grid-template-columns: 60% 40%;
 	margin-top: 30px;
+	grid-gap: 50px;
 }
 </style>
